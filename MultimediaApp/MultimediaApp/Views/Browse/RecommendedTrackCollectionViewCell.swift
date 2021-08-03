57a2518b300020937423ace44af41859de9444c4
Igor Penkin <69014236+Igor-A-Penkin@users.noreply.github.com>
2021-08-03 16:21:55 +0300

    Added formatted TimeLabel for Recommended tracks

--- MultimediaApp/MultimediaApp/Views/Browse/RecommendedTrackCollectionViewCell.swift
+++ MultimediaApp/MultimediaApp/Views/Browse/RecommendedTrackCollectionViewCell.swift
@@ -33,10 +33,11 @@ class RecommendedTrackCollectionViewCell: UICollectionViewCell {
     }()
     
     private let durationLabel: UILabel = {
-        let artistNameLabel = UILabel()
-        artistNameLabel.font = .systemFont(ofSize: 14, weight: .regular)
-        artistNameLabel.numberOfLines = 1
-        return artistNameLabel
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        label.textAlignment = .right
+        label.numberOfLines = 1
+        return label
     }()
     
     override init(frame: CGRect) {
@@ -56,19 +57,20 @@ class RecommendedTrackCollectionViewCell: UICollectionViewCell {
         super.layoutSubviews()
         let offset: CGFloat = 5
         let imageSize: CGFloat = contentView.height
+        let durationWidth: CGFloat = 48
         albumCoverImageView.frame = CGRect(x: offset,
                                            y: 0,
                                            width: imageSize,
                                            height: imageSize)
         trackNameLabel.frame = CGRect(x: albumCoverImageView.right+offset*2,
                                       y: 0,
-                                      width: contentView.width-imageSize-offset*4,
+                                      width: contentView.width-imageSize-durationWidth-offset*4,
                                       height: imageSize/2)
         artistNameLabel.frame = CGRect(x: albumCoverImageView.right+offset*2,
                                        y: trackNameLabel.bottom,
-                                       width: trackNameLabel.width,
+                                       width: trackNameLabel.width-durationWidth,
                                        height: imageSize/2)
-        durationLabel.frame = CGRect(x: <#T##CGFloat#>, y: <#T##CGFloat#>, width: <#T##CGFloat#>, height: <#T##CGFloat#>)
+        durationLabel.frame = CGRect(x: width-durationWidth-offset, y: imageSize/4, width: durationWidth, height: imageSize/2)
     }
     
     override func prepareForReuse() {
@@ -82,7 +84,14 @@ class RecommendedTrackCollectionViewCell: UICollectionViewCell {
         trackNameLabel.text = viewModel.name
         artistNameLabel.text = viewModel.artistName
         albumCoverImageView.sd_setImage(with: viewModel.artworkURL, completed: nil)
-        durationLabel.text = "000:00"
+        let totalSeconds = (viewModel.duration/1000)
+        let minutes = totalSeconds/60
+        let seconds = totalSeconds%60
+        if (seconds <= 9) {
+            durationLabel.text = "\(minutes):0\(seconds)"
+        } else {
+            durationLabel.text = "\(minutes):\(seconds)"
+        }
     }
     
 }
