a9347389c5b23cea3dc97fb372f065d6b12095a8
Igor Penkin <69014236+Igor-A-Penkin@users.noreply.github.com>
2021-09-03 13:30:53 +0300

    Added Welcome animation

--- MultimediaApp/MultimediaApp/Controllers/Registration/WelcomeViewController.swift
+++ MultimediaApp/MultimediaApp/Controllers/Registration/WelcomeViewController.swift
@@ -18,37 +18,96 @@ class WelcomeViewController: UIViewController {
         return button
     }()
     
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "albumCovers")
+        return imageView
+    }()
+    
+    private let overlayView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.systemBackground.withAlphaComponent(0.7)
+        return view
+    }()
+    
+    private let promoView: UIView = {
+        let view = UIView(frame: CGRect(x: 0,
+                                        y: 0,
+                                        width: 280,
+                                        height: 280))
+        view.layer.opacity = 0
+        let imageView = UIImageView(frame: CGRect(x: 0,
+                                                  y: 0,
+                                                  width: view.width,
+                                                  height: view.height/2))
+        imageView.image = UIImage(named: "originalSpotify")
+        imageView.contentMode = .scaleAspectFit
+        let label = UILabel(frame: CGRect(x: 10,
+                                          y: imageView.bottom+10,
+                                          width: view.width-20,
+                                          height: view.height-imageView.height-20))
+        label.text = "Built with Spotify API.\nOverview project on\ngithub.com/Igor-A-Penkin"
+        label.font = UIFont.systemFont(ofSize: 22, weight: .semibold)
+        label.numberOfLines = 3
+        label.textAlignment = .center
+        view.addSubview(imageView)
+        view.addSubview(label)
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         Logger.log(object: Self.self, method: #function)
-        title = "Spotify"
+        title = "Muse.me"
         view.backgroundColor = .systemBackground
+        view.addSubview(imageView)
+        view.addSubview(overlayView)
+        view.addSubview(promoView)
         view.addSubview(signInButton)
         signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        imageView.frame = view.bounds
+        overlayView.frame = view.bounds
+        promoView.center = CGPoint(x: view.width+promoView.width/2,
+                                   y: view.center.y)
         signInButton.frame = CGRect(x: 30,
                                     y: view.height-90-view.safeAreaInsets.bottom,
                                     width: view.width-60, height: 60)
         signInButton.layer.cornerRadius = signInButton.height/2
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 1.0, delay: 0.5, usingSpringWithDamping: 0.6, initialSpringVelocity: 1, options: .curveEaseOut) {
+            self.promoView.layer.opacity = 1
+            self.promoView.center = self.view.center
+        } completion: {_ in }
+    }
+    
     @objc func didTapSignIn() {
         Logger.log(object: Self.self, method: #function)
-        let authViewController = AuthViewController()
-        authViewController.navigationItem.largeTitleDisplayMode = .never
-        authViewController.completionHandler = { [weak self] success in
-            guard self != nil else {
-                print("INFO: Error - WelcomeViewController was unexpectedly nil.")
-                return
+        UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.1, options: .curveEaseInOut) {
+            self.promoView.layer.opacity = 0
+            self.promoView.center = CGPoint(x: -self.view.width,
+                                            y: self.view.center.y)
+        } completion: {_ in
+            let authViewController = AuthViewController()
+            authViewController.navigationItem.largeTitleDisplayMode = .never
+            authViewController.completionHandler = { [weak self] success in
+                guard self != nil else {
+                    print("INFO: Error - WelcomeViewController was unexpectedly nil.")
+                    return
+                }
+                DispatchQueue.main.async {
+                    self?.handleSignIn(success: success)
+                }
             }
-            DispatchQueue.main.async {
-                self?.handleSignIn(success: success)
-            }
+            self.navigationController?.pushViewController(authViewController, animated: true)
         }
-        navigationController?.pushViewController(authViewController, animated: true)
     }
 
     private func handleSignIn(success: Bool) {
